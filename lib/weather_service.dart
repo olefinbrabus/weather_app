8d821a900faa6cccb33668aa3635e89553def370
Oleg Filipenkov <149054884+olefinbrabus@users.noreply.github.com>
2024-10-03 22:29:47 +0300

    Initial

--- lib/weather_service.dart
+++ lib/weather_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
-  final String apiKey = '8432c495d830aa5e47251c3d7b19d069';
+  final String apiKey = '';
   final String baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
   Future<Map<String, dynamic>> getWeather(String city) async {
